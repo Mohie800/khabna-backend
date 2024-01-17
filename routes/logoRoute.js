1b167cce140ccad19b4f5d9600f80edb3d27e29e
mohie800 <mohy800@gmail.com>
2024-01-17 23:12:48 +0300

    uplaod logo

--- routes/logoRoute.js
+++ routes/logoRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const logoController = require("../controllers/logoController");
 const upload = require("../middleware/upload");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/get", async (req, res) => {
   try {
@@ -20,24 +21,15 @@ router.post("/create", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
-router.post(
-  "/update",
-  authMiddleware,
-  upload.single("file"),
-  async (req, res) => {
-    let url = null;
+router.post("/update", authMiddleware, async (req, res) => {
+  let url = null;
 
-    if (req.file) {
-      url = `https://kind-ruby-worm-boot.cyclic.app/storage/${req.file.filename}`;
-    }
-
-    try {
-      const logo = await logoController.Edit(req.body.id, url);
-      res.json(logo);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+  try {
+    const logo = await logoController.Edit(req.body.id, req.body.url);
+    res.json(logo);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
-);
+});
 
 module.exports = router;
